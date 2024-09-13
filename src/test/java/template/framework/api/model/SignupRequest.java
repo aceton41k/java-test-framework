0d68bbbebc556f12efc4b8a7ed01bb4b2ef92b38
Pavel Saushkin <aceton41k@gmail.com>
2024-09-14 01:51:07 +0700

    SignupRequest.java

---
+++ src/test/java/template/framework/api/model/SignupRequest.java
@@ -0,0 +1,14 @@
+package template.framework.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class SignupRequest {
+    private String email;
+    private String password;
+    private String fullName;
+}
