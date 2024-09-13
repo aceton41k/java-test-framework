0e0b6de50f3a3c73792a07179c6354935ece7539
Pavel Saushkin <aceton41k@gmail.com>
2024-09-13 18:48:42 +0700

    reconfigure jooq generated classes and skip generation when run test

--- src/test/generated/jooq/Tables.java
+++ src/test/generated/jooq/Tables.java
@@ -13,7 +13,7 @@ import jooq.tables.Users;
 /**
  * Convenience access to all tables in public.
  */
-@SuppressWarnings({ "all", "unchecked", "rawtypes" })
+@SuppressWarnings({ "all", "unchecked", "rawtypes", "this-escape" })
 public class Tables {
 
     /**
